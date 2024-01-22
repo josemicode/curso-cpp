0289cfeb21f97fe25671bfb30ea1b257a2999342
Jose <95041130+josemicode@users.noreply.github.com>
2024-01-22 09:36:21 +0100

    Delete tema5/agendatelefonica directory
    duplicated

--- tema5/agendatelefonica/cabecera.h
+++
@@ -1,9 +0,0 @@
-using namespace std;
-typedef struct{
-	int DNI;
-	string Apellidos;
-	string Nombre;
-	int Telefono;
-}registro;
-void nuevoElemento(registro agenda[], int n);
-void mostrarAgenda(registro agenda[], int n);
