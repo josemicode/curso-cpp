e54e8514159b5949c6f1bd369176be9c3579e7a8
josemicode <jmiguelojedagutierrez@gmail.com>
2024-04-26 10:50:25 +0200

    Format Jugador.h

--- tema9/Jugador.h
+++ tema9/Jugador.h
@@ -1,24 +1,27 @@
-#include<iostream>
+#include <iostream>
 #pragma once
 
 using namespace std;
 
-class Jugador{
-	protected:
-		string nombre;
-		float vida;
-		float ataque;
-	private:
-		static float base_vida;
-		static float base_ataque;
-	public:
-		Jugador();
-		Jugador(string nombre, float vida, float ataque);
-		string getNombre();
-		float getVida();
-		float getAtaque();
-		virtual void setVidaAtaque() = 0;
-		virtual void activarPoder() = 0;
-		virtual void recibirAtaque(float danho);
-		virtual string toString();
+class Jugador
+{
+protected:
+	string nombre;
+	float vida;
+	float ataque;
+
+private:
+	static float base_vida;
+	static float base_ataque;
+
+public:
+	Jugador();
+	Jugador(string nombre, float vida, float ataque);
+	string getNombre();
+	float getVida();
+	float getAtaque();
+	virtual void setVidaAtaque() = 0;
+	virtual void activarPoder() = 0;
+	virtual void recibirAtaque(float danho);
+	virtual string toString();
 };
