60fe7989024e3d31dd177343808b1e9645917a48
josemicode <jmiguelojedagutierrez@gmail.com>
2024-05-31 22:21:06 +0200

    ft principal.cpp

--- tema14/principal.cpp
+++ tema14/principal.cpp
@@ -1,46 +1,51 @@
-#include<iostream>
-#include<array>
-#include<vector>
-#include<set>
-#include<list>
+#include <iostream>
+#include <array>
+#include <vector>
+#include <set>
+#include <list>
 
-#include"Cuenta.h"
-#include"Cliente.h"
+#include "Cuenta.h"
+#include "Cliente.h"
 
 using namespace std;
 
-template<typename container> int encontrarIndice(container clientes, string DNI);
+template <typename container>
+int encontrarIndice(container clientes, string DNI);
 
-
-istream& operator>>(istream& is, Cliente& cliente) {
-	return  is >> dato;
+istream &operator>>(istream &is, Cliente &cliente)
+{
+	return is >> dato;
 }
 
-istream& operator>>(istream& is, Cuenta& cuenta) {
-	return  is >> dato;
+istream &operator>>(istream &is, Cuenta &cuenta)
+{
+	return is >> dato;
 }
 
-ostream& operator<<(ostream& os, const Cliente& cliente) {
+ostream &operator<<(ostream &os, const Cliente &cliente)
+{
 	return os << dato;
 }
 
-ostream& operator<<(ostream& os, const Cuenta& cuenta) {
+ostream &operator<<(ostream &os, const Cuenta &cuenta)
+{
 	return os << dato;
 }
 
-
-int main() {
-
+int main()
+{
 
 	return 0;
 }
 
-
-template<typename container> int encontrarIndice(container clientes, string DNI) {
+template <typename container>
+int encontrarIndice(container clientes, string DNI)
+{
 	int indice = -1, busqueda = 0;
-	for (const auto& c : clientes)
+	for (const auto &c : clientes)
 	{
-		if (c == DNI) {
+		if (c == DNI)
+		{
 			indice = busqueda;
 			break;
 		}
