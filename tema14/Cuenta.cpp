4c1804d6440b05536f9819c90b879cd270a3771c
josemicode <jmiguelojedagutierrez@gmail.com>
2024-05-31 22:20:43 +0200

    ft Cuenta.cpp

--- tema14/Cuenta.cpp
+++ tema14/Cuenta.cpp
@@ -1,12 +1,14 @@
-#include"Cuenta.h"
-Cuenta::Cuenta(){
-
+#include "Cuenta.h"
+Cuenta::Cuenta()
+{
 }
-Cuenta::Cuenta(const Cuenta& otra){
+Cuenta::Cuenta(const Cuenta &otra)
+{
     this->numero = otra.numero;
     this->saldo = otra.saldo;
     this->nro_credito = otra.nro_credito;
 }
-bool Cuenta::operator<(const Cuenta& otra) const{
+bool Cuenta::operator<(const Cuenta &otra) const
+{
     return stol(this->numero.substr(15)) < stol(otra.numero.substr(15));
 }
