38f6101b7bbed66b58277bc8757e26d21ce99249
josemicode <jmiguelojedagutierrez@gmail.com>
2024-05-31 22:48:30 +0200

    modified:   tema14/principal.cpp

--- tema14/principal.cpp
+++ tema14/principal.cpp
@@ -1,55 +1,112 @@
 #include <iostream>
-#include <array>
-#include <vector>
+#include <fstream>
 #include <set>
-#include <list>
+#include <string>
+#include <iomanip>
 
-#include "Cuenta.h"
 #include "Cliente.h"
+#include "Cuenta.h"
 
 using namespace std;
 
-template <typename container>
-int encontrarIndice(container clientes, string DNI);
+template <typename Container>
+int encontrarIndice(const Container &clientes, const string &DNI);
 
-istream &operator>>(istream &is, Cliente &cliente)
-{
-	return is >> dato;
-}
+void leerClientes(const string &archivoClientes, set<Cliente> &clientes);
+void leerCuentas(const string &archivoCuentas, set<Cliente> &clientes);
 
-istream &operator>>(istream &is, Cuenta &cuenta)
-{
-	return is >> dato;
-}
-
-ostream &operator<<(ostream &os, const Cliente &cliente)
-{
-	return os << dato;
-}
-
-ostream &operator<<(ostream &os, const Cuenta &cuenta)
-{
-	return os << dato;
-}
+void imprimirClientes(const set<Cliente> &clientes);
+void crearResumenCuentas(const set<Cliente> &clientes);
 
 int main()
 {
+    set<Cliente> clientes;
 
-	return 0;
+    leerClientes("clientes.txt", clientes);
+    leerCuentas("cuentas2023.txt", clientes);
+
+    imprimirClientes(clientes);
+    crearResumenCuentas(clientes);
+
+    return 0;
 }
 
-template <typename container>
-int encontrarIndice(container clientes, string DNI)
+template <typename Container>
+int encontrarIndice(const Container &clientes, const string &DNI)
 {
-	int indice = -1, busqueda = 0;
-	for (const auto &c : clientes)
-	{
-		if (c == DNI)
-		{
-			indice = busqueda;
-			break;
-		}
-		busqueda++;
-	}
-	return indice;
+    int indice = -1, busqueda = 0;
+    for (const auto &c : clientes)
+    {
+        if (c == DNI)
+        {
+            indice = busqueda;
+            break;
+        }
+        busqueda++;
+    }
+    return indice;
+}
+
+void leerClientes(const string &archivoClientes, set<Cliente> &clientes)
+{
+    ifstream archivo(archivoClientes);
+    string nombre, DNI;
+
+    while (archivo >> nombre >> DNI)
+    {
+        Cliente cliente(nombre, DNI);
+        clientes.insert(cliente);
+    }
+}
+
+void leerCuentas(const string &archivoCuentas, set<Cliente> &clientes)
+{
+    ifstream archivo(archivoCuentas);
+    string DNI, numero;
+    float saldo;
+    int nro_credito;
+
+    while (archivo >> DNI >> numero >> saldo >> nro_credito)
+    {
+        Cuenta cuenta;
+        cuenta.numero = numero;
+        cuenta.saldo = saldo;
+        cuenta.nro_credito = nro_credito;
+
+        for (auto &cliente : clientes)
+        {
+            if (cliente == DNI)
+            {
+                cliente.agregarCuenta(cuenta);
+                int indice = encontrarIndice(clientes, DNI);
+                cout << "Al cliente " << setw(3) << setfill('0') << indice << " se ha agregado la cuenta con numero " << numero << endl;
+                break;
+            }
+        }
+    }
+}
+
+void imprimirClientes(const set<Cliente> &clientes)
+{
+    for (const auto &cliente : clientes)
+    {
+        cout << left << setw(30) << setfill(' ') << cliente.getNombre()
+             << setw(12) << cliente.getDNI()
+             << right << setw(3) << cliente.getCuentas().size() << endl;
+    }
+}
+
+void crearResumenCuentas(const set<Cliente> &clientes)
+{
+    ofstream resumen("Resumen de Cuentas.txt");
+    for (const auto &cliente : clientes)
+    {
+        for (const auto &cuenta : cliente.getCuentas())
+        {
+            resumen << cuenta.numero << ", "
+                    << cuenta.saldo << ", "
+                    << cliente.getDNI() << ", "
+                    << cliente.getNombre() << endl;
+        }
+    }
 }
