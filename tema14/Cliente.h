be6d97e73a22e35696a1bbc5ed4f5e0a1183a639
josemicode <jmiguelojedagutierrez@gmail.com>
2024-05-31 22:19:50 +0200

    ft Cliente.h

--- tema14/Cliente.h
+++ tema14/Cliente.h
@@ -3,22 +3,23 @@
 
 #include "Cuenta.h"
 
-class Cliente{
-    private:
-        string nombre;
-        string DNI;
-        set<Cuenta> cuentas;
+class Cliente
+{
+private:
+    string nombre;
+    string DNI;
+    set<Cuenta> cuentas;
 
-    public:
-        Cliente();
-        Cliente(string nombre, string DNI);
+public:
+    Cliente();
+    Cliente(string nombre, string DNI);
 
-        void agregarCuenta(Cuenta& cuenta);
-        set<Cuenta> getCuentas();
-        string getNombre();
+    void agregarCuenta(Cuenta &cuenta);
+    set<Cuenta> getCuentas();
+    string getNombre();
 
-        bool operator<(const Cliente& otro) const;
-        bool operator==(const string& DNI) const;
-        friend ostream& operator<<(ostream &os,const Cliente& cliente);
-        friend istream& operator>>(istream &is, Cliente& cliente);
+    bool operator<(const Cliente &otro) const;
+    bool operator==(const string &DNI) const;
+    friend ostream &operator<<(ostream &os, const Cliente &cliente);
+    friend istream &operator>>(istream &is, Cliente &cliente);
 };
