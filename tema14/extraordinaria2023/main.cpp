86fd56a1c5cdbfaf0a7a679d593f89ac74526504
PCRDO\walls <wallstreetdoggo@gmail.com>
2024-06-06 13:58:24 +0200

    Rf main

--- tema14/extraordinaria2023/main.cpp
+++ tema14/extraordinaria2023/main.cpp
@@ -14,78 +14,95 @@ ifstream f_conductores = ifstream("conductores.txt", ios::in);
 ofstream f_resumen = ofstream("Resumen_Conductores.txt", ios::out);
 ofstream f_coches_asig = ofstream("conductores_coches.txt", ios::out);
 
-ostream& operator<<(ostream& os, Coche& coche) {
-	if(&os == &cout){
+ostream &operator<<(ostream &os, Coche &coche)
+{
+	if (&os == &cout)
+	{
 		os << right << setw(7) << coche.getMarca() << " - " << coche.getModelo() << " " << left << setw(5) << coche.getAnho() << " " << coche.getMatricula();
-	}else{
+	}
+	else
+	{
 		os << coche.getMatricula() << "," << coche.getMarca() << "," << coche.getModelo(); // TODO: Implement outstream for files (should be accessed by Conductor's os on a condition)
 	}
 	return os;
 }
-istream& operator>>(istream& is, Coche& coche) {
+istream &operator>>(istream &is, Coche &coche)
+{
 	is >> coche.matricula >> coche.marca >> coche.modelo >> coche.anho;
 	return is;
 }
-ostream& operator<<(ostream& os, Conductor& conductor) {
+ostream &operator<<(ostream &os, Conductor &conductor)
+{
 	// se recomienda verificar si el conductor tiene un coche asociado verificando si es diferente que nullptr
-		if(&os == &cout){
+	if (&os == &cout)
+	{
 		os << right << setw(15) << conductor.getDNI() << " " << left << setw(5) << conductor.getNumViajes() << " " << right << setw(10) << conductor.getPuntuacion();
-	}else{
-		if(&os == &f_resumen){
-
-		}else if (&os == &f_coches_asig)
+	}
+	else
+	{
+		if (&os == &f_resumen)
 		{
-
 		}
-		
+		else if (&os == &f_coches_asig)
+		{
+		}
 	}
 	return os;
 }
-istream& operator>>(istream& is, Conductor& conductor) {
+istream &operator>>(istream &is, Conductor &conductor)
+{
 	is >> conductor.DNI >> quoted(conductor.nombre_completo) >> conductor.num_viajes >> conductor.puntuacion;
 	return is;
 }
 
-int main() {
+int main()
+{
 	// Variables necesarias
 	vector<Coche> coches;
 	vector<Conductor> conductores;
 	/* Contenedor Vector
-	* Agregar un elemento a un vector
-	* 1. nombre_vector.push_back(Clase(...));
-	* 2. Clase clase;
-	*    nombre_vector.push_back(clase);
-	* Acceder a un elemento de un array
-	*    nombre_vector.at(indice)
-	* Obtener el tama�o de un vector
-	*    nombre_vector.size()
-	*/
+	 * Agregar un elemento a un vector
+	 * 1. nombre_vector.push_back(Clase(...));
+	 * 2. Clase clase;
+	 *    nombre_vector.push_back(clase);
+	 * Acceder a un elemento de un array
+	 *    nombre_vector.at(indice)
+	 * Obtener el tama�o de un vector
+	 *    nombre_vector.size()
+	 */
 	int opcion = 0;
 
 	// Variables opcionales
 	int cantidad_conductores_asignados = 0;
 
-
-	cout << "Bienvenido a la empresa Coches VTC!" << endl << "Seleccione una opcion numerica:" << endl;
-	while (opcion != 8) {
+	cout << "Bienvenido a la empresa Coches VTC!" << endl
+		<< "Seleccione una opcion numerica:" << endl;
+	while (opcion != 8)
+	{
 		mostrarMenu();
 
 		cin >> opcion;
-		switch (opcion) {
-		case 1: {
+		switch (opcion)
+		{
+		case 1:
+		{
 			// Cargar conductores desde un archivo
 			cout << "Cargando todos los conductores de conductores.txt" << endl;
 			if (f_conductores)
 			{
 				Conductor aux_conductor;
-				while (f_conductores >> aux_conductor){
+				while (f_conductores >> aux_conductor)
+				{
 					conductores.push_back(aux_conductor);
 				}
 			}
-			
-			cout << endl << "-------------------------------------------------" << endl;
-			break; }
-		case 2: {
+
+			cout << endl
+				<< "-------------------------------------------------" << endl;
+			break;
+		}
+		case 2:
+		{
 			// Cargar coches desde un archivo
 			cout << "Cargando todos los coches de coches.txt" << endl;
 			if (f_coches)
@@ -93,71 +110,98 @@ int main() {
 				Coche aux_coche;
 				string cabecera;
 				f_coches >> cabecera >> cabecera >> cabecera >> cabecera;
-				while (f_coches >> aux_coche){
+				while (f_coches >> aux_coche)
+				{
 					coches.push_back(aux_coche);
 				}
 			}
-			
-			cout << endl << "-------------------------------------------------" << endl;
-			break; }
-		case 3: {
+
+			cout << endl
+				<< "-------------------------------------------------" << endl;
+			break;
+		}
+		case 3:
+		{
 			// Asignar coches a los conductores
 			cout << "Se asignaran " << coches.size() << " coches entre " << conductores.size() << " diferentes conductores." << endl;
-			
-			for(int i = 0; i < coches.size(); i++){
+
+			for (int i = 0; i < coches.size(); i++)
+			{
 				conductores.at(i).setCoche(&coches.at(i));
 			}
 
-			cout << endl << "-------------------------------------------------" << endl;
-			break; }
-		case 4: {
+			cout << endl
+				<< "-------------------------------------------------" << endl;
+			break;
+		}
+		case 4:
+		{
 			// Mostrar los datos de todos los coches
 			cout << "Existen " << coches.size() << " coches, mostrados a continuacion: " << endl;
 
-			for(int i = 0; i < coches.size(); i++){
+			for (int i = 0; i < coches.size(); i++)
+			{
 				cout << coches.at(i) << endl;
 			}
 
-			cout << endl << "-------------------------------------------------" << endl;
-			break; }
-		case 5: {
+			cout << endl
+				<< "-------------------------------------------------" << endl;
+			break;
+		}
+		case 5:
+		{
 			// Mostrar los datos de todos los conductores
 			cout << "Existen " << conductores.size() << " conductores, mostrados a continuacion: " << endl;
 
-			for(int i = 0; i < conductores.size(); i++){
+			for (int i = 0; i < conductores.size(); i++)
+			{
 				cout << conductores.at(i) << endl;
 			}
-			
-			cout << endl << "-------------------------------------------------" << endl;
-			break; }
-		case 6: {
+
+			cout << endl
+				<< "-------------------------------------------------" << endl;
+			break;
+		}
+		case 6:
+		{
 			// Crear un archivo con los datos de todos los conductores
 			cout << "A continuacion se creara un archivo con todos los conductores, mostrando si/no tiene un coche." << endl;
-			cout << endl << "-------------------------------------------------" << endl;
-			break; }
-		case 7: {
+			cout << endl
+				<< "-------------------------------------------------" << endl;
+			break;
+		}
+		case 7:
+		{
 			// Crear un archivo con los datos de todos los conductores y sus coches asignados
 			cout << "A continuacion se creara un archivo con todos los conductores, informando de su coche asignado si lo hubiere." << endl;
-			cout << endl << "-------------------------------------------------" << endl;
-			break; }
-		case 8: {
+			cout << endl
+				<< "-------------------------------------------------" << endl;
+			break;
+		}
+		case 8:
+		{
 			// Salir
-			break; }
-		default: {
-			cout << endl << "-------------------------------------------------" << endl;
+			break;
+		}
+		default:
+		{
+			cout << endl
+				<< "-------------------------------------------------" << endl;
 			cout << "Opcion no valida" << endl;
-			cout << endl << "-------------------------------------------------" << endl;
-			break; }
+			cout << endl
+				<< "-------------------------------------------------" << endl;
+			break;
+		}
 		}
 	}
 
 	cout << "Adios!" << endl;
 
-
 	return 0;
 }
 
-void mostrarMenu() {
+void mostrarMenu()
+{
 	cout << "1. Cargar conductores desde un archivo" << endl;
 	cout << "2. Cargar coches desde un archivo" << endl;
 	cout << "3. Asignar coches a los conductores" << endl;
